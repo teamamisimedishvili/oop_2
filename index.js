5cef441f13858308e29c6a9734bd3d450f95eaff
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-02-21 11:01:57 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+class Rectangle {
+    height;
+     width;
+
+     Rect_ange() {
+        document.write (this.height);
+        document.write (this.width);
+    
+     }
+}
+var obieqti = new Rectangle ()
+
+obieqti.height= "set_height " + "</br>";
+obieqti.width = "set_width";
+
+obieqti.Rect_ange()
